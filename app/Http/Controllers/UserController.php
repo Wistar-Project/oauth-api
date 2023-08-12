2489e5b133e0bf9df5098885e069877cb6aafc29
fabrisdev <ezepuntocom@hotmail.com>
2023-08-12 20:13:45 -0300

    Correcciones de programación

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,8 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Auth;
-use Lcobucci\JWT\Parser;
 use Illuminate\Support\Facades\Validator;
 use App\Models\Persona;
 use App\Models\Administrador;
@@ -14,31 +12,28 @@ use App\Models\Conductor;
 use App\Models\Funcionario; 
 use Illuminate\Support\Facades\DB;
 
-
-
-
 class UserController extends Controller
 {
     private function validarDatos($request){
         return Validator::make($request->all(),[
             'email' => 'required|email|unique:users',
-            'rol'=> 'required',
+            'rol'=> 'required|in:administrador,funcionario,conductor',
             'nombre'=>'required|max:50',
             'apellido'=>'required|max:50',
             'password' => 'required|confirmed'
         ]);
-        
     }
+
     public function Register(Request $request){
         $validacion = $this -> validarDatos($request); 
-        if($validacion->fails()){
+        if($validacion -> fails()){
             $BAD_REQUEST_HTTP=400;
             abort($BAD_REQUEST_HTTP, $validacion->errors());
         }
         DB::transaction(function() use($request){
-            $usuario= $this -> createUser($request);
-            $this ->createPersona($request,$usuario->id);
-            $this ->addRoleToPersona($usuario ->id,$request->rol);
+            $usuario = $this -> createUser($request);
+            $this -> createPersona($request,$usuario->id);
+            $this -> addRoleToPersona($usuario ->id,$request->rol);
         });
     }
 
@@ -55,44 +50,41 @@ class UserController extends Controller
     }
 
     public function Logout(Request $request){
-        $request->user()->token()->revoke();
+        $request -> user() -> token() -> revoke();
         return ['message' => 'Token Revoked'];
     }
 
     private function createPersona($request ,$id){
         $persona = new Persona();
-        $persona -> nombre= $request ->post('nombre');
-        $persona -> apellido= $request ->post('apellido');
-        $persona -> id= $id;
+        $persona -> nombre = $request -> post('nombre');
+        $persona -> apellido = $request -> post('apellido');
+        $persona -> id = $id;
         $persona -> save();
     }
     
     private function createConductor($id){
         $conductor = new Conductor();
-        $conductor -> id= $id;
+        $conductor -> id = $id;
         $conductor-> save();
     }
     
     private function createAdministrador($id){
         $administrador = new Administrador();
-        $administrador -> id= $id;
+        $administrador -> id = $id;
         $administrador-> save();
     }
   
     private function createFuncionario($id){
         $funcionario = new Funcionario();
-        $funcionario -> id= $id;
+        $funcionario -> id = $id;
         $funcionario-> save();
     }
     private function addRoleToPersona($id,$rol){
-        $BAD_REQUEST_HTTP=400;
         if($rol == 'administrador')
             return $this -> createAdministrador($id);
         if($rol == 'conductor')
             return $this -> createConductor($id);
-        if($rol == 'funcionario')
-            return $this -> createFuncionario($id);
-        abort($BAD_REQUEST_HTTP,"El rol especificado no es valido");
+        return $this -> createFuncionario($id);
     }
 
     public function MostrarPersona(){
