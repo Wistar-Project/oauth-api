01bb5d2e5bd7784d35bb597e250e68cb537f11d8
fabrisdev <ezepuntocom@hotmail.com>
2023-11-12 18:22:32 -0300

    Tests de user test arreglados

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -2,6 +2,10 @@
 
 namespace Tests\Feature;
 
+use App\Models\Administrador;
+use App\Models\Persona;
+use App\Models\User;
+use Hash;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -18,19 +22,31 @@ class UserTest extends TestCase
      *
      * @return void
      */
-    public function test_ObtenerTokenConClientIdValido()
+    public function test_iniciar_sesion_con_client_valido()
     {
+        $usuarioTesting = User::factory() -> create([
+            "email" => "persona@email.com",
+            "password" => Hash::make("contra1234")
+        ]);
+        Persona::factory() -> create([
+            "id" => $usuarioTesting -> id,
+            "nombre" => "Martin",
+            "apellido" => "Lopez"
+        ]);
+        Administrador::factory() -> create([
+            "id" => $usuarioTesting -> id
+        ]);
+
         Artisan::call('passport:client',[
             '--password' => true,
             '--no-interaction'=>true,
             '--name'=>'Test Client',
         ]);        
-            
-        $client = Client::findOrFail(1);
+        $client = Client::all()->first();
 
         $response = $this->post('/oauth/token',[
-            "username" => "usuario@usuario",
-            "password" => "1234",
+            "username" => "persona@email.com",
+            "password" => "contra1234",
             "grant_type" => "password",
             "client_id" => "1",
             "client_secret" => $client -> secret
@@ -50,13 +66,13 @@ class UserTest extends TestCase
 
     }
 
-    public function test_ObtenerTokenConClientIdInvalido()
+    public function test_iniciar_sesion_con_client_invalido()
     {
          
         $response = $this->post('/oauth/token',[
             "grant_type" => "password",
-            "client_id" => "234",
-            "client_secret" => "sdfsdfsdf"
+            "client_id" => "999",
+            "client_secret" => "qafasgf"
         ]);
 
         $response->assertStatus(401);
@@ -67,63 +83,49 @@ class UserTest extends TestCase
         ]);
     }
 
-    public function test_ValidarTokenSinEnviarToken()
+    public function test_iniciar_sesion()
     {
-        $response = $this->get('/api/v1/validate');
-        $response->assertStatus(500);
-        
-    }
-
-    public function test_ValidarTokenConTokenInvalido()
-    {
-        $response = $this->get('/api/v1/validate',[
-            [ "Authorization" => "Token Roto"]
+        $usuarioTesting = User::factory() -> create([
+            "email" => "usuario@usuario",
+            "password" => Hash::make("1234")
         ]);
-        $response->assertStatus(500);
-        
-    }
+        Persona::factory() -> create([
+            "id" => $usuarioTesting -> id,
+            "nombre" => "pepe",
+            "apellido" => "hola"
+        ]);
+        Administrador::factory() -> create([
+            "id" => $usuarioTesting -> id
+        ]);
 
-    public function test_ValidarTokenConTokenValido()
-    {
-        $client = Client::findOrFail(1);
-        $tokenResponse = $this -> post("/oauth/token",[
+        $client = Client::all()->first();
+        $response = $this -> post("/oauth/token",[
             "username" => "usuario@usuario",
             "password" => "1234",
             "grant_type" => "password",
             "client_id" => "1",
             "client_secret" => $client -> secret
         ]);
-
-        $token = json_decode($tokenResponse -> content(),true);
-
-        $response = $this->get('/api/v1/validate',
-            [ "Authorization" => "Bearer " . $token ['access_token']]
-        );
-
-
         $response->assertStatus(200);
-        
     }
 
-    public function test_LogoutSinToken()
+    public function test_cerrar_sesion_sin_autenticarse()
     {
         $response = $this->get('/api/v1/logout');
         $response->assertStatus(500);
-        
     }
 
-    public function test_LogoutConTokenInvalido()
+    public function test_cerrar_sesion_con_token_invalido()
     {
         $response = $this->get('/api/v1/logout',[
             [ "Authorization" => "Token Roto"]
         ]);
         $response->assertStatus(500);
-        
     }
 
-    public function test_LogoutConTokenValido()
+    public function test_cerrar_sesion()
     {
-        $client = Client::findOrFail(1);
+        $client = Client::all()->first();
         $tokenResponse = $this -> post("/oauth/token",[
             "username" => "usuario@usuario",
             "password" => "1234",
@@ -137,14 +139,9 @@ class UserTest extends TestCase
         $response = $this->get('/api/v1/logout',
             [ "Authorization" => "Bearer " . $token ['access_token']]
         );
-
-
         $response->assertStatus(200);
         $response->assertJsonFragment(
-            ['message' => 'Token Revoked']
+            ['message' => 'Sesión cerrada satisfactoriamente']
         );
-        
     }
-
-
 }
