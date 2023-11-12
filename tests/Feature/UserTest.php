73afa5cf11356f12a0db7797d3552de7d9a58419
fabrisdev <ezepuntocom@hotmail.com>
2023-11-12 18:45:03 -0300

    Testing listo

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -3,6 +3,8 @@
 namespace Tests\Feature;
 
 use App\Models\Administrador;
+use App\Models\Conductor;
+use App\Models\Funcionario;
 use App\Models\Persona;
 use App\Models\User;
 use Hash;
@@ -125,23 +127,85 @@ class UserTest extends TestCase
 
     public function test_cerrar_sesion()
     {
+        $user = User::factory() -> create([
+            "email" => "diego@gmail.com",
+            "password" => Hash::make("contra1234")
+        ]);
+        Persona::factory() -> create([
+            "id" => $user -> id,
+            "nombre" => "Diego",
+            "apellido" => "Rodriguez"
+        ]);
+        Administrador::factory() -> create([
+            "id" => $user -> id
+        ]);
         $client = Client::all()->first();
-        $tokenResponse = $this -> post("/oauth/token",[
+
+        $response = $this -> post("/oauth/token",[
             "username" => "usuario@usuario",
             "password" => "1234",
             "grant_type" => "password",
             "client_id" => "1",
             "client_secret" => $client -> secret
         ]);
-
-        $token = json_decode($tokenResponse -> content(),true);
-        
+        $response->assertStatus(200);
+        $token = json_decode($response -> content(),true);
         $response = $this->get('/api/v1/logout',
             [ "Authorization" => "Bearer " . $token ['access_token']]
         );
         $response->assertStatus(200);
-        $response->assertJsonFragment(
+        $response->assertExactJson(
             ['message' => 'Sesión cerrada satisfactoriamente']
         );
+    }
+
+    public function test_obtener_rol(){
+        $user = User::factory() -> create([
+            "email" => "sofiaperez02@gmail.com",
+            "password" => Hash::make("contra1234")
+        ]);
+        Persona::factory() -> create([
+            "id" => $user -> id,
+            "nombre" => "Sofia",
+            "apellido" => "Perez"
+        ]);
+        Funcionario::factory() -> create([
+            "id" => $user -> id
+        ]);
+        $response = $this->actingAs($user, "api")->get('/api/v1/rol');
+        $response->assertStatus(200);
+        $response->assertSeeText("funcionario");
+    }
+
+    public function test_obtener_rol_sin_autenticarse(){
+        $response = $this->get('/api/v1/rol', [
+            "Accept" => "application/json"
+        ]);
+        $response->assertStatus(401);
+    }
+
+    public function test_obtener_nombre(){
+        $user = User::factory() -> create([
+            "email" => "otrasofiaperez@gmail.com",
+            "password" => Hash::make("contra1234")
+        ]);
+        Persona::factory() -> create([
+            "id" => $user -> id,
+            "nombre" => "Sofia",
+            "apellido" => "Perez"
+        ]);
+        Conductor::factory() -> create([
+            "id" => $user -> id
+        ]);
+        $response = $this->actingAs($user, "api")->get('/api/v1/nombre');
+        $response->assertStatus(200);
+        $response->assertSeeText("Sofia Perez");
+    }
+    
+    public function test_obtener_nombre_sin_autenticarse(){
+        $response = $this->get('/api/v1/nombre', [
+            "Accept" => "application/json"
+        ]);
+        $response->assertStatus(401);
     }
 }
